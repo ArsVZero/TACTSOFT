4a05e385cebc2bb2d2546c07a2f4f7064259b7cb
MARS II <ars.nsu1994@gmail.com>
2023-02-23 19:59:41 +0600

    Delete Program to Count the Number of Digits directory

--- Program to Count the Number of Digits/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Enter an integer: ");
-int integer = Convert.ToInt32(Console.ReadLine());
-int number = integer, count = 0;
-while (integer != 0)
-{
-    integer /= 10;
-    count++;
-}
-Console.WriteLine("Number of digits in {0} is {1}", number, count);
