e99e3938fed77cc85cf61cfaa84506f41d731b8e
MARS II <ars.nsu1994@gmail.com>
2023-02-23 20:08:04 +0600

    Delete Swap Numbers Without Using Temporary Variables directory

--- Swap Numbers Without Using Temporary Variables/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Enter first value: ");
-int firstValue = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter second value: ");
-int secondValue = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Before swapping, first value is {0} & second value is {1}", firstValue, secondValue);
-firstValue = firstValue + secondValue;
-secondValue = firstValue - secondValue;
-firstValue = firstValue - secondValue;
-Console.WriteLine("After swapping, first value is {0} & second value is {1}", firstValue, secondValue);
